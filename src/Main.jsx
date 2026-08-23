7c9b9b88e60a1632fe6c77bd8ca334637ee71667
oussama1603 <oussamadz1603@gmail.com>
2026-08-23 01:34:56 +0100

    !

--- src/Main.jsx
+++ src/Main.jsx
@@ -24,9 +24,19 @@ export default function Main(){
         name = 'ingredient'/>
         <button>+ Add ingredient</button>
       </form>
-      <ul>
-        {ingredientElements}
-      </ul>
+     {ingredients.length > 0 && <section>
+    <h2>Ingredients on hand:</h2>
+    <ul className="ingredients-list" aria-live="polite">
+      {ingredientElements}
+    </ul>
+      <div className="get-recipe-container">
+        <div>
+          <h3>Ready for a recipe?</h3>
+          <p>Generate a recipe from your list of ingredients.</p>
+        </div>
+        <button>Get a recipe</button>
+      </div>
+</section>}
     </main>
   )
 }
